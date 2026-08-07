e464555ea2c7319b5f7f47617306e91309b704b0
lemuel-manske <lkmliz@Rute.localdomain>
2026-08-07 20:32:38 -0300

    fix: run ansible as current user

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,32 +2,34 @@
 
 set -euo pipefail
 
-if ! command -v git &>/dev/null || ! command -v ansible &>/dev/null; then
-  echo "==> Updating apt..."
+REPO_URL="https://github.com/lemuel-manske/.lkmliz.git"
+REPO_DIR="${HOME}/.files"
 
-  sudo apt-get update -y
+if ! command -v git &>/dev/null || ! command -v ansible &>/dev/null; then
+    echo "==> Updating apt..."
+    sudo apt-get update -y
 fi
 
 if ! command -v git &>/dev/null; then
-  echo "==> Installing git..."
-
-  sudo apt-get install -y git
+    echo "==> Installing git..."
+    sudo apt-get install -y git
 fi
 
 if ! command -v ansible &>/dev/null; then
-  echo "==> Installing ansible..."
-
-  sudo apt-get install -y ansible
+    echo "==> Installing ansible..."
+    sudo apt-get install -y ansible
 fi
 
-if [ ! -d "$HOME/.files" ]; then
-  echo "==> Cloning .files..."
-
-  git clone https://github.com/lemuel-manske/.files.git "$HOME/.files"
+if [ ! -d "${REPO_DIR}" ]; then
+    echo "==> Cloning .lkmliz..."
+    git clone "${REPO_URL}" "${REPO_DIR}"
 fi
 
 echo "==> Installing Galaxy collections..."
-sudo ansible-galaxy collection install -r "$HOME/.files/ansible/requirements.yml"
+ansible-galaxy collection install \
+    -r "${REPO_DIR}/ansible/requirements.yml"
 
 echo "==> Running playbook..."
-sudo ansible-playbook "$HOME/.files/ansible/playbook.yml" -i "$HOME/.files/ansible/inventory"
+ansible-playbook \
+    "${REPO_DIR}/ansible/playbook.yml" \
+    -i "${REPO_DIR}/ansible/inventory"
